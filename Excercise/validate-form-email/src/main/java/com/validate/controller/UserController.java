f350303e524f0e76a2f73446759d7f0a702a6551
Compass <ndphong165@gmail.com>
2022-10-24 17:21:59 +0700

    done BT1

---
+++ Excercise/validate-form-email/src/main/java/com/validate/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.validate.controller;
+
+import com.validate.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class UserController {
+    @GetMapping("/user")
+    public String showForm(Model model) {
+        model.addAttribute("user", new User());
+        return "index";
+    }
+    @PostMapping("/user")
+    public String checkValidation(@Valid @ModelAttribute("user")User user,
+                                  BindingResult bindingResult, Model model) {
+        new User().validate(user, bindingResult);
+        if (bindingResult.hasFieldErrors()) {
+            return "index";
+        } else {
+            model.addAttribute("user", user); {
+                return "result";
+            }
+        }
+    }
+}
